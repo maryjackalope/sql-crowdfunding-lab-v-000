b2361562d752715bd48c66eb98fdbaf43f7feae6
Mary Jack <maryjackinthebox@gmail.com>
2018-06-13 16:27:27 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,8 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY, 
+name TEXT, 
+quantity INTEGER, 
+aisle INTEGER);
  
 INSERT INTO groceries VALUES (1, "Apples", 2, 1);
 INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
